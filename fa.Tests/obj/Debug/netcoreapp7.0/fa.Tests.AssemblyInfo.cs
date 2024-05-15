3c53dec49411340cf7c7a7ba1b5e0f2ee916a06e
Egor Logutov <sv9to0slav@gmail.com>
2024-05-15 14:56:11 +0300

    main

--- fa.Tests/obj/Debug/netcoreapp7.0/fa.Tests.AssemblyInfo.cs
+++ fa.Tests/obj/Debug/netcoreapp7.0/fa.Tests.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("fa")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("fa.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338d1ae3c536c42669241040a31cbc77c65a8a80")]
-[assembly: System.Reflection.AssemblyProductAttribute("fa")]
-[assembly: System.Reflection.AssemblyTitleAttribute("fa")]
+[assembly: System.Reflection.AssemblyProductAttribute("fa.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("fa.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- fa/program.cs
+++ fa/program.cs
@@ -6,52 +6,193 @@ using System.Threading.Tasks;
 
 namespace fans
 {
-  public class State
-  {
-    public string Name;
-    public Dictionary<char, State> Transitions;
-    public bool IsAcceptState;
-  }
-
-
-  public class FA1
-  {
-    public bool? Run(IEnumerable<char> s)
+    public class State
     {
-      return false;
+        public string Name;
+        public Dictionary<char, State> Transitions;
+        public bool IsAcceptState;
     }
-  }
 
-  public class FA2
-  {
-    public bool? Run(IEnumerable<char> s)
+    public class FA1
     {
-      return false;
-    }
-  }
-  
-  public class FA3
-  {
-    public bool? Run(IEnumerable<char> s)
-    {
-      return false;
-    }
-  }
+        public static State start = new State()
+        {
+            Name = "start",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State oneZero = new State()
+        {
+            Name = "oneZero",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State oneZeroOne = new State()
+        {
+            Name = "oneZeroOne",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State invalid = new State()
+        {
+            Name = "invalid",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
 
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      String s = "01111";
-      FA1 fa1 = new FA1();
-      bool? result1 = fa1.Run(s);
-      Console.WriteLine(result1);
-      FA2 fa2 = new FA2();
-      bool? result2 = fa2.Run(s);
-      Console.WriteLine(result2);
-      FA3 fa3 = new FA3();
-      bool? result3 = fa3.Run(s);
-      Console.WriteLine(result3);
+        State InitialState = start;
+
+        public FA1()
+        {
+            start.Transitions['0'] = oneZero;
+            start.Transitions['1'] = start;
+
+            oneZero.Transitions['0'] = invalid;
+            oneZero.Transitions['1'] = oneZeroOne;
+
+            oneZeroOne.Transitions['0'] = invalid;
+            oneZeroOne.Transitions['1'] = oneZeroOne;
+
+            invalid.Transitions['0'] = invalid;
+            invalid.Transitions['1'] = invalid;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s)
+            {
+                if (!current.Transitions.ContainsKey(c))
+                    return null;
+                current = current.Transitions[c];
+            }
+            return current.IsAcceptState;
+        }
     }
-  }
+
+    public class FA2
+    {
+        public static State evenZerosEvenOnes = new State()
+        {
+            Name = "evenZerosEvenOnes",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State oddZerosEvenOnes = new State()
+        {
+            Name = "oddZerosEvenOnes",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State evenZerosOddOnes = new State()
+        {
+            Name = "evenZerosOddOnes",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State oddZerosOddOnes = new State()
+        {
+            Name = "oddZerosOddOnes",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState = evenZerosEvenOnes;
+
+        public FA2()
+        {
+            evenZerosEvenOnes.Transitions['0'] = oddZerosEvenOnes;
+            evenZerosEvenOnes.Transitions['1'] = evenZerosOddOnes;
+
+            oddZerosEvenOnes.Transitions['0'] = evenZerosEvenOnes;
+            oddZerosEvenOnes.Transitions['1'] = oddZerosOddOnes;
+
+            evenZerosOddOnes.Transitions['0'] = oddZerosOddOnes;
+            evenZerosOddOnes.Transitions['1'] = evenZerosEvenOnes;
+
+            oddZerosOddOnes.Transitions['0'] = evenZerosOddOnes;
+            oddZerosOddOnes.Transitions['1'] = oddZerosEvenOnes;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s)
+            {
+                if (!current.Transitions.ContainsKey(c))
+                    return null;
+                current = current.Transitions[c];
+            }
+            return current.IsAcceptState;
+        }
+    }
+
+    public class FA3
+    {
+        public static State start = new State()
+        {
+            Name = "start",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State seenOne = new State()
+        {
+            Name = "seenOne",
+            IsAcceptState = false,
+            Transitions = new Dictionary<char, State>()
+        };
+        public static State seenTwoOnes = new State()
+        {
+            Name = "seenTwoOnes",
+            IsAcceptState = true,
+            Transitions = new Dictionary<char, State>()
+        };
+
+        State InitialState = start;
+
+        public FA3()
+        {
+            start.Transitions['0'] = start;
+            start.Transitions['1'] = seenOne;
+
+            seenOne.Transitions['0'] = start;
+            seenOne.Transitions['1'] = seenTwoOnes;
+
+            seenTwoOnes.Transitions['0'] = seenTwoOnes;
+            seenTwoOnes.Transitions['1'] = seenTwoOnes;
+        }
+
+        public bool? Run(IEnumerable<char> s)
+        {
+            State current = InitialState;
+            foreach (var c in s)
+            {
+                if (!current.Transitions.ContainsKey(c))
+                    return null;
+                current = current.Transitions[c];
+            }
+            return current.IsAcceptState;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s1 = "0111";
+            FA1 fa1 = new FA1();
+            bool? result1 = fa1.Run(s1);
+            Console.WriteLine(result1);
+
+            String s2 = "0001";
+            FA2 fa2 = new FA2();
+            bool? result2 = fa2.Run(s2);
+            Console.WriteLine(result2);
+
+            String s3 = "1101";
+            FA3 fa3 = new FA3();
+            bool? result3 = fa3.Run(s3);
+            Console.WriteLine(result3);
+        }
+    }
 }
